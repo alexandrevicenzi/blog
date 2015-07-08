31583195d3b6a6de8de919fe9a6e2ac8815c3400
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-07-08 01:09:06 -0300

    Fix HTTPS url.

--- publishconf.py
+++ publishconf.py
@@ -3,15 +3,14 @@
 
 from __future__ import unicode_literals
 
-# This file is only used if you use `make publish` or
-# explicitly specify it as your config file.
-
 import os
 import sys
+
 sys.path.append(os.curdir)
+
 from pelicanconf import *
 
-SITEURL = 'http://blog.alexandrevicenzi.com'
+SITEURL = '//blog.alexandrevicenzi.com'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
