3fa1048d4133e1ceffcad3519ff45a7cc605699a
Alexandre Vicenzi <vicenzi.alexandre@gmail.com>
2015-07-07 22:38:16 -0300

    Initial server setup.

---
+++ fabfile.py
@@ -0,0 +1,65 @@
+from fabric.api import *
+import fabric.contrib.project as project
+import os
+import shutil
+import sys
+import SocketServer
+
+from pelican.server import ComplexHTTPRequestHandler
+
+# Local path configuration (can be absolute or relative to fabfile)
+env.deploy_path = 'output'
+DEPLOY_PATH = env.deploy_path
+
+# Github Pages configuration
+env.github_pages_branch = "gh-pages"
+
+# Port for `serve`
+PORT = 8000
+
+def clean():
+    """Remove generated files"""
+    if os.path.isdir(DEPLOY_PATH):
+        shutil.rmtree(DEPLOY_PATH)
+        os.makedirs(DEPLOY_PATH)
+
+def build():
+    """Build local version of site"""
+    local('pelican -s pelicanconf.py')
+
+def rebuild():
+    """`clean` then `build`"""
+    clean()
+    build()
+
+def regenerate():
+    """Automatically regenerate site upon file modification"""
+    local('pelican -r -s pelicanconf.py')
+
+def serve():
+    """Serve site at http://localhost:8000/"""
+    os.chdir(env.deploy_path)
+
+    class AddressReuseTCPServer(SocketServer.TCPServer):
+        allow_reuse_address = True
+
+    server = AddressReuseTCPServer(('', PORT), ComplexHTTPRequestHandler)
+
+    sys.stderr.write('Serving on port {0} ...\n'.format(PORT))
+    server.serve_forever()
+
+def reserve():
+    """`build`, then `serve`"""
+    build()
+    serve()
+
+def preview():
+    """Build production version of site"""
+    local('pelican -s publishconf.py')
+
+def publish():
+    """Publish to GitHub Pages"""
+    clean()
+    local('pelican -s publishconf.py')
+    local("ghp-import -b {github_pages_branch} {deploy_path}".format(**env))
+    local("git push origin {github_pages_branch}".format(**env))
---
+++ pelicanconf.py
@@ -0,0 +1,40 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
+
+from __future__ import unicode_literals
+
+AUTHOR = u'Alexandre Vicenzi'
+SITENAME = u"Alexandre's Blog"
+SITEURL = '/'
+TAGLINE = 'Systems Analyst - Maker'
+USER_LOGO_URL = '//alexandrevicenzi.com/img/profile.png'
+
+THEME = './pelican-svbhack'
+
+PATH = 'content'
+
+TIMEZONE = 'America/Sao_Paulo'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+USE_FOLDER_AS_CATEGORY = True
+
+# Blogroll
+LINKS = (('About', '//alexandrevicenzi.com/'),
+         ('Buteco Open Source', '//butecopensource.org/'),)
+
+# Social widget
+SOCIAL = (('Linkedin', 'https://br.linkedin.com/in/alexandrevicenzi/en'),
+          ('GitHub', 'https://github.com/alexandrevicenzi'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+#RELATIVE_URLS = True
